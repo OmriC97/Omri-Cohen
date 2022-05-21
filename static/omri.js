b0a4558b82d5075c4991eb9ab1a2dba0479ceb06
Omri Cohen <omrc@post.bgu.ac.il>
2022-05-21 23:51:58 +0300

    excersi 2

---
+++ static/omri.js
@@ -0,0 +1,15 @@
+
+
+function thankAlert(){
+    alert('Thank you for participating')
+}
+
+function changeColor() {
+    var elem = window.document.getElementById('currTab');
+    console.log(elem);
+    // elem.style.color = newColor;
+}
+
+changeColor();
+
+
