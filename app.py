ddc6377f53bc31c0968e8330acc038df1d575d78
Omri Cohen <omrc@post.bgu.ac.il>
2022-06-25 16:24:42 +0300

    Test

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def Home_page():
+    return render_template('home.html')
+
+@app.route('/contact')
+def Contact_Page():
+    return render_template('contact.html')
+
+@app.route('/home')
+def Home_page2():
+    return render_template('home.html')
+
+
+@app.route('/assignment3_1')
+def assignment3_1():
+    return render_template('assignment3_1.html',Hobbies = ['Basketball','Soccer','friends','Resturants'], Favorite_NBA_Players = ['Kobe', 'MJ', 'Lebron','Luka', 'Shaquil'])
+
+@app.route('/assignment3_1/redirect')
+def assignment3_1_redirect():
+    return redirect('/assignment3_1')
+
+@app.route('/assignment3_1/url_for')
+def assignment3_1_url_for():
+    return redirect(url_for('assignment3_1'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
